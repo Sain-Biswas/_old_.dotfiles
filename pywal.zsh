793b9a5bf0d89b65beab63a5f702fd46579db363
Sain-Biswas <sain.biswas614@gmail.com>
2025-02-05 11:56:54 +0530

    Add Jetbrains IntelliJ Idea to transparent window rulez

--- pywal.zsh
+++ pywal.zsh
@@ -1,8 +1,8 @@
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#5a5f6f,bg:#101216,spinner:#c3c3c4,hl:#5F5E71 \
---color=fg:#c3c3c4,header:#5F5E71,info:#9F97A0,pointer:#c3c3c4 \
---color=marker:#7E7D87,fg+:#c3c3c4,prompt:#9F97A0,hl+:#5F5E71 \
---color=selected-bg:#101216 \
+--color=bg+:#59656d,bg:#0D1214,spinner:#c2c3c4,hl:#2B7B82 \
+--color=fg:#c2c3c4,header:#2B7B82,info:#D1BAAA,pointer:#c2c3c4 \
+--color=marker:#9C9A9F,fg+:#c2c3c4,prompt:#D1BAAA,hl+:#2B7B82 \
+--color=selected-bg:#0D1214 \
 --multi"
 
-zstyle ':fzf-tab:*' fzf-flags --color=bg+:#5a5f6f,bg:#101216,spinner:#c3c3c4,hl:#5F5E71,fg:#c3c3c4,header:#5F5E71,info:#9F97A0,pointer:#c3c3c4,marker:#7E7D87,fg+:#c3c3c4,prompt:#9F97A0,hl+:#5F5E71,selected-bg:#101216 --multi
+zstyle ':fzf-tab:*' fzf-flags --color=bg+:#59656d,bg:#0D1214,spinner:#c2c3c4,hl:#2B7B82,fg:#c2c3c4,header:#2B7B82,info:#D1BAAA,pointer:#c2c3c4,marker:#9C9A9F,fg+:#c2c3c4,prompt:#D1BAAA,hl+:#2B7B82,selected-bg:#0D1214 --multi
