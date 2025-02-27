c5aefa2479d21c3bcbd93a28dc33794af143d088
Sain-Biswas <sain.biswas614@gmail.com>
2025-02-27 19:13:12 +0530

    Add i3

--- pywal.zsh
+++ pywal.zsh
@@ -1,8 +1,8 @@
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#575f6b,bg:#0a0d11,spinner:#c1c2c3,hl:#56586E \
---color=fg:#c1c2c3,header:#56586E,info:#78869C,pointer:#c1c2c3 \
---color=marker:#67708A,fg+:#c1c2c3,prompt:#78869C,hl+:#56586E \
---color=selected-bg:#0a0d11 \
+--color=bg+:#5b6670,bg:#121518,spinner:#c3c4c5,hl:#B2A35C \
+--color=fg:#c3c4c5,header:#B2A35C,info:#C5A5BC,pointer:#c3c4c5 \
+--color=marker:#68B6C5,fg+:#c3c4c5,prompt:#C5A5BC,hl+:#B2A35C \
+--color=selected-bg:#121518 \
 --multi"
 
-zstyle ':fzf-tab:*' fzf-flags --color=bg+:#575f6b,bg:#0a0d11,spinner:#c1c2c3,hl:#56586E,fg:#c1c2c3,header:#56586E,info:#78869C,pointer:#c1c2c3,marker:#67708A,fg+:#c1c2c3,prompt:#78869C,hl+:#56586E,selected-bg:#0a0d11 --multi
+zstyle ':fzf-tab:*' fzf-flags --color=bg+:#5b6670,bg:#121518,spinner:#c3c4c5,hl:#B2A35C,fg:#c3c4c5,header:#B2A35C,info:#C5A5BC,pointer:#c3c4c5,marker:#68B6C5,fg+:#c3c4c5,prompt:#C5A5BC,hl+:#B2A35C,selected-bg:#121518 --multi
